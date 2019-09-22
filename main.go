0c5cc3fbc16f623d5e31a62b55344bc64b43a0ba
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-09-22 17:21:01 +0300

    wp 8

--- main.go
+++ main.go
@@ -5,68 +5,49 @@ import (
     "time"
 
     "github.com/alecrabbit/go-cli-spinner"
-    "github.com/mattn/go-runewidth"
-    "github.com/rivo/uniseg"
 )
 
 func main() {
-    i := spinner.Snake3
-    fmt.Println()
-    fmt.Println()
-    printCharSet([]string{"←", "↖", "↑", "↗", "→", "↘", "↓", "↙",})
-    printCharSet([]string{"■", "□", "▪", "▫"})
-    fmt.Println()
-
-    printCharSet([]string{"👪", "뢴", "🇩🇪", "ö", "🏳️‍🌈",})
-    printCharSet([]string{"🏳️‍🌈", "🇨🇴", "🇧🇼"})
-    // for a, _ := range spinner.CharSets {
-    //     printCharSet(spinner.CharSets[a])
-    //     fmt.Println()
-    // }
     messages := []string{
-        "Initializing",
         "Starting",
-        "Long message: this message continues further",
+        "Initializing",
         "Gathering data",
-        "Short",
-        "Short",
+        "Checking data",
+        "Checking weather",
         "Processing",
-        "Process",
         "Processing",
+        "Processing",
+        "Processing",
+        "Processing",
+        "Processing",
+        "Processing",
+        "Processing",
+        "Almost there",
+        "Be patient",
     }
-    fmt.Println("Open > ")
-    s := spinner.New(i, 100*time.Millisecond)
-    s.FinalMSG = "Done!\n"
+
+    s := spinner.New(spinner.Snake3, 100*time.Millisecond)
+    s.FinalMessage = "Done!\n"
     // s.HideCursor = false
+
     s.Start()
     l := len(messages)
     for i, m := range messages {
-    	s.Erase()
-    	fmt.Println(m)
-    	fmt.Print(".......")
-    	s.Last()
-    	s.Message(time.Now().Format("15:04:05"))
-        f := float32(i) / float32(l)
-        // fmt.Printf("%4.0f%%\n", f)
-    	s.Progress(f)
-	    time.Sleep(5 * time.Second)
-    }
-	time.Sleep(1 * time.Second)
-	s.Stop()
-    fmt.Println("Finished")
-
-}
-
-func printCharSet(aw []string) {
-        for _, c := range aw {
-            width := runewidth.StringWidth(c)
-
-            fmt.Printf("%s %v ", c, width)
-            gr := uniseg.NewGraphemes(c)
-            for gr.Next() {
-                fmt.Printf("%x ", gr.Runes())
-            }
-            fmt.Println()
+        // Printing execution message
+        {
+            s.Erase()
+            fmt.Println(m)
+            s.Last()
         }
-        fmt.Println()
+        // Simulating spinner message
+        s.Message(fmt.Sprintf("Message at %s", time.Now().Format("15:04:05")))
+        // Simulating spinner progress
+        f := float32(i) / float32(l)
+        s.Progress(f)
+        // Doing some work
+        time.Sleep(1 * time.Second)
+    }
+    time.Sleep(3 * time.Second)
+    s.Stop()
+    // fmt.Println("Finished")
 }
