f49509ecc27ed8ccf758cc14dec8f262ffb1847e
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-10-03 17:04:46 +0300

    Auto stash before rebase of "develop"

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
     "fmt"
+    "log"
     "math/rand"
     "time"
-    "log"
 
     "github.com/davecgh/go-spew/spew"
     // "github.com/pkg/profile"
@@ -23,22 +23,19 @@ func main() {
     // defer profile.Start(profile.CPUProfile, profile.ProfilePath("./profiling")).Stop()
     // defer profile.Start(profile.MemProfile, profile.MemProfileRate(1), profile.ProfilePath("./profiling")).Stop()
 
-    messages := []string{
-        "Starting",
-        "Initializing",
-        "Gathering data",
-        "Checking data",
-        "Checking weather",
-        "Processing",
-        "Processing",
-        "Processing",
-        "Processing",
-        "Processing",
-        "Processing",
-        "Processing",
-        "Processing",
-        "Almost there",
-        "Be patient",
+    messages := map[int]string{
+        0:   "Starting",
+        3:   "Initializing",
+        6:   "Gathering data",
+        9:  "Processing",
+        16:  "Processing",
+        25:  "Processing",
+        44:  "Processing",
+        60:  "Processing",
+        79:  "Processing",
+        82:  "Still processing",
+        90:  "Be patient",
+        95:  "Almost there",
     }
 
     s, err := spinner.New(
@@ -47,7 +44,7 @@ func main() {
         spinner.ColorLevel(color.TColor256),
         spinner.ProgressFormat("[%4s]"), // [  7%]
         spinner.MessageFormat("(%s)"),   // (message)
-        spinner.Format("<%s >"),         // (message)
+        // spinner.Format("%s "),         //
     )
     if err != nil {
         log.Fatal(err)
@@ -64,41 +61,40 @@ func main() {
     fmt.Print("..................................")
     // Start spinner
     s.Start()
-    // for _, m := range messages {
-    l := len(messages)
-    for i, m := range messages {
+    for i := 0; i <= 100; i++ {
         // Doing some work 1
         time.Sleep(duration())
-        // Printing execution message
-        {
-            s.Erase() // optional if you're absolutely sure that your messages are longer
-            fmt.Println(m)
-            fmt.Print("..................................") // string to show that spinner can be used in inline mode
-            s.Current()                                     // Write current frame to output(optional - for smooth amination)
+        if m, ok := messages[i]; ok {
+            // Printing execution message
+            {
+                s.Erase() // optional if you're absolutely sure that your messages are longer
+                fmt.Println(m)
+                fmt.Print("..................................") // string to show that spinner can be used in inline mode
+                s.Current()                                     // Write current frame to output(optional - for smooth amination)
+            }
+
         }
         // Simulating spinner message
-        if rand.Intn(16) > 9 {
+        if i > 10 && i < 20 || i > 40 && i < 60 {
             s.Message("") // Sometimes there are no messages
         } else {
-            s.Message(fmt.Sprintf("Message at %s", time.Now().Format("15:04:05")))
+            s.Message(fmt.Sprintf("Status message at %s", time.Now().Format("15:04:05")))
         }
         // Doing some work 2
         time.Sleep(duration())
         // Simulating spinner progress
-        f := float32(i) / float32(l)
-        if f > 0.5 && f < 0.7 {
+        if i > 50 && i < 70 {
             s.Progress(0)
         } else {
-            s.Progress(f) // float32 0..1
+            s.Progress(float32(i) / float32(100)) // float32 0..1
         }
+
     }
-    time.Sleep(1 * time.Second)
     // Stop spinner
     s.Stop()
-    time.Sleep(1 * time.Second)
-
+    time.Sleep(500 * time.Millisecond)
 }
 
 func duration() time.Duration {
-    return (200 + time.Duration(rand.Intn(1600))) * time.Millisecond
+    return (50 + time.Duration(rand.Intn(50))) * time.Millisecond
 }
